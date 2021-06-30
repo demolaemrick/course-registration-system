955ba3ac80eec7a2fed2d843d7ded81105f475af
Emrick19 <emricky17@gmail.com>
2021-06-30 23:28:24 +0100

    Code refactor

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,16 @@
 import "reflect-metadata";
-import {createConnection} from "typeorm";
-import {User} from "./entity/User";
+import { createConnection } from "typeorm";
+import express from "express";
 
-createConnection().then(async connection => {
+const app = express();
+app.use(express.json());
 
-    console.log("Inserting a new user into the database...");
-    const user = new User();
-    user.firstName = "Timber";
-    user.lastName = "Saw";
-    user.age = 25;
-    await connection.manager.save(user);
-    console.log("Saved a new user with id: " + user.id);
-
-    console.log("Loading users from the database...");
-    const users = await connection.manager.find(User);
-    console.log("Loaded users: ", users);
-
-    console.log("Here you can setup and run express/koa/any other framework.");
-
-}).catch(error => console.log(error));
+createConnection()
+  .then(() => {
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => {
+        console.log(`Server running at http://localhost:${PORT}`)
+    })
+    
+  })
+  .catch((error) => console.log(error));
