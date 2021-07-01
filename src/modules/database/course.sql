e8ccd5e3d88aa3f143ac78aa9b35a77cbf12ac41
Emrick19 <emricky17@gmail.com>
2021-07-01 22:05:32 +0100

    Create an sql file to insert values into db

---
+++ src/modules/database/course.sql
@@ -0,0 +1,11 @@
+INSERT INTO courses ("course_code", "course_title", "course_unit") VALUES
+('CSC101', 'Introduction To Computer Science', 2),
+('GST111', 'Communication In English', 2),
+('GST121', 'Nigeria People and Culture', 2),
+('MAT101', 'General Mathematics', 3),
+('CSC107', 'Introduction To Computer Science (Practical)', 1),
+('STA111', 'Statistics', 3),
+('PHY101', 'General Physics', 2),
+('CHM101', 'General Chemistry', 2),
+('PHY107', 'General Physics (Practical)', 1),
+('CHM107', 'General Chemistry(Pratical)', 1)
