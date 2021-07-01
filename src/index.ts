94dc9747a43387b005752cc886d7a7fce23dcda5
Emrick19 <emricky17@gmail.com>
2021-07-01 22:03:43 +0100

    Import cors

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
 import "reflect-metadata";
 import { createConnection } from "typeorm";
 import express from "express";
+import cors from "cors"
 
 const app = express();
+
 app.use(express.json());
+app.use(cors())
+
+app.use
 
 createConnection()
   .then(() => {
