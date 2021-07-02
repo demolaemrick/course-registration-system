77d6abbe2e7e6d6381aa41d6da2f213c3cb94985
Emrick19 <emricky17@gmail.com>
2021-07-02 14:59:54 +0100

    Import user routes

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,14 @@ import { createConnection } from "typeorm";
 import express from "express";
 import cors from "cors"
 
+import userRoutes from "./modules/routes/user.route"
+
 const app = express();
 
 app.use(express.json());
 app.use(cors())
 
-app.use
+app.use('/api/user', userRoutes)
 
 createConnection()
   .then(() => {
