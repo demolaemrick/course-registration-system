f324ccc7f792a4e4db771da17b42fdd6b2560589
Emrick19 <emricky17@gmail.com>
2021-07-02 15:00:14 +0100

    Update User entity

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,27 +1,30 @@
-import {Entity, Column} from "typeorm";
-import Model from "./Model"
+import { Entity, Column } from "typeorm";
+import Model from "./Model";
 
 @Entity("users")
 export default class User extends Model {
-    @Column()
-    firstName: string;
+  @Column()
+  firstName: string;
 
-    @Column()
-    lastName: string;
+  @Column()
+  lastName: string;
 
-    @Column()
-    email: string;
-    
-    @Column()
-    level: string;
-    
-    @Column()
-    phone: number;
+  @Column()
+  email: string;
 
-    @Column()
-    matric_no: string;
+  @Column({ nullable: true })
+  level: string;
 
-    @Column()
-    password: string;
+  @Column({ nullable: true })
+  phone: number;
 
+  @Column()
+  matric_no: string;
+
+  @Column()
+  password: string;
+
+  toJSON() {
+    return { ...this, password: undefined, id: undefined };
+  }
 }
