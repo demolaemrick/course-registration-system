7b48b501af326e3a4f9d478cc074366d900bbec4
Emrick19 <emricky17@gmail.com>
2021-07-02 21:32:29 +0100

    Add course routes

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import express from "express";
 import cors from "cors"
 
 import userRoutes from "./modules/routes/user.route"
+import courseRoutes from "./modules/routes/course.route"
 
 const app = express();
 
@@ -11,6 +12,7 @@ app.use(express.json());
 app.use(cors())
 
 app.use('/api/user', userRoutes)
+app.use('/api/courses', courseRoutes)
 
 createConnection()
   .then(() => {
