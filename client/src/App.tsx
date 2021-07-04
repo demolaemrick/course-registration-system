f2da1f67b0b1ddf9496d088fe0f350e8f2914abc
Emrick19 <emricky17@gmail.com>
2021-07-04 22:20:16 +0100

    Add Authentication route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import Layout from "./components/Layout/Layout";
 import Home from "./components/pages/Home"
+import Auth from "./components/pages/Auth"
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Switch>
         <Layout>          
           <Route exact path='/' component={Home} />
+          <Route path='/login' component={Auth} />
         </Layout>
       </Switch>
     </Router>
