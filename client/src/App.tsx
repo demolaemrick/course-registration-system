d860bcf8246b018e5e7faf3dacd305a4f144cf6e
Emrick19 <emricky17@gmail.com>
2021-07-04 21:20:37 +0100

    Routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,17 @@
-import Layout from "./components/Layout/Layout"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import Layout from "./components/Layout/Layout";
+import Home from "./components/pages/Home"
 
 function App() {
   return (
-    <div className="App">
-      <Layout>
-        <h1>React app</h1>
-      </Layout>
-    </div>
+    <Router>
+      <Switch>
+        <Layout>          
+          <Route exact path='/' component={Home} />
+        </Layout>
+      </Switch>
+    </Router>
   );
 }
 
