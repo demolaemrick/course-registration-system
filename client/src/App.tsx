d8e6ac45c56ea61f0bb1ddb2b723f3657dd969ca
Emrick19 <emricky17@gmail.com>
2021-07-05 22:14:14 +0100

    Add more imports

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,20 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import Layout from "./components/Layout/Layout";
-import Home from "./components/pages/Home"
-import Auth from "./components/pages/Auth"
+import Home from "./components/pages/Home";
+import Login from "./components/pages/Login";
+import SignUp from "./components/pages/SignUp";
+import Profile from "./components/pages/Profile";
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Layout>          
-          <Route exact path='/' component={Home} />
-          <Route path='/login' component={Auth} />
+        <Route path="/register" component={SignUp} />
+        <Route path="/login" component={Login} />
+        <Layout>
+          <Route exact path="/" component={Home} />
+          <Route path="/profile" component={Profile} />
         </Layout>
       </Switch>
     </Router>
