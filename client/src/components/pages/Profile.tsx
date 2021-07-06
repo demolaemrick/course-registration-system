294e28d831628844d860543c834c1c3daf770803
Emrick19 <emricky17@gmail.com>
2021-07-06 19:07:25 +0100

    Update Profile page

--- client/src/components/pages/Profile.tsx
+++ client/src/components/pages/Profile.tsx
@@ -17,26 +17,42 @@ const Profile = () => {
     <Center h="300px">
       <Card width="50%">
         <Flex>
-          <FormControl id="matricNo">
-            <HStack>
-              <FormLabel>Matric No:</FormLabel>
-              <Input
-                maxWidth="50%"
-                type="text"
-                focusBorderColor="teal.200"
-                placeholder="Enter your matric number"
-                size="sm"
-              />
-            </HStack>
-          </FormControl>
+          <Box w="600px">
+            <FormControl id="matricNo" mb="3">
+              <HStack>
+                <FormLabel>Matric No</FormLabel>
+                <Input
+                  maxWidth="50%"
+                  type="text"
+                  focusBorderColor="teal.200"
+                  placeholder="Enter your matric number"
+                  size="sm"
+                />
+              </HStack>
+            </FormControl>
+            <FormControl id="matricNo">
+              <HStack>
+                <FormLabel>Matric No</FormLabel>
+                <Input
+                  maxWidth="50%"
+                  type="text"
+                  focusBorderColor="teal.200"
+                  placeholder="Enter your matric number"
+                  size="sm"
+                />
+              </HStack>
+            </FormControl>
+          </Box>
 
           <Spacer />
-          <Image
-            boxSize="150px"
-            objectFit="cover"
-            src="https://bit.ly/dan-abramov"
-            alt="Dan Abramov"
-          />
+          <Box>
+            <Image
+              boxSize="150px"
+              objectFit="cover"
+              src="https://bit.ly/dan-abramov"
+              alt="Dan Abramov"
+            />
+          </Box>
         </Flex>
       </Card>
     </Center>
