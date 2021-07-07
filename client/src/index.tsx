e7deaa1fcc535f57009fd457228689a41c884731
Emrick19 <emricky17@gmail.com>
2021-07-07 21:16:43 +0100

    Add store provider

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { ChakraProvider } from "@chakra-ui/react";
+import { store } from "./store";
+import { Provider } from "react-redux";
 
 import "./index.css";
 import App from "./App";
@@ -8,7 +10,9 @@ import App from "./App";
 ReactDOM.render(
   <React.StrictMode>
     <ChakraProvider>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </ChakraProvider>
   </React.StrictMode>,
   document.getElementById("root")
