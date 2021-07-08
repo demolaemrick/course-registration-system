8af05f4d5c22fdfae345c61c5b9e3752eab22e70
Emrick19 <emricky17@gmail.com>
2021-07-08 18:18:35 +0100

    Query courses from reducer

--- client/src/components/pages/Home.tsx
+++ client/src/components/pages/Home.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import { Flex, Center } from "@chakra-ui/react";
+import { useSelector } from "react-redux";
+import { RootState } from "../../store";
 
 import Table from "../UI/Table";
 
 const Home = () => {
+  const { courses } = useSelector((state: RootState) => state.courseReducer);
   return (
     <React.Fragment>
       <Flex justify="center" align="center" h="400px">
         <Center w="80%">
-          <Table />
+          {courses.length > 0 && <Table courses={courses} />}
         </Center>
       </Flex>
     </React.Fragment>
