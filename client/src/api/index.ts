036d27452cd3efe91892f05c7f6176d0c7e94ad7
Emrick19 <emricky17@gmail.com>
2021-07-08 22:46:22 +0100

    Add register route

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -1,5 +1,10 @@
 import axios from 'axios';
+import { userFormData } from "../types/user";
 
 const API = axios.create({ baseURL: 'http://localhost:5000/api' });
 
+//courses
 export const fetchCourses = () => API.get(`/courses`);
+
+//authentication
+export const registerUser = (userData: userFormData) => API.post('/user/register', userData)
