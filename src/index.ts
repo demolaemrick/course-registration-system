bf117de11ef45923624eb0a7dc96118ada13dffe
Emrick19 <emricky17@gmail.com>
2021-07-09 22:04:04 +0100

    Add cookieParser as middleware

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import "reflect-metadata";
 import { createConnection } from "typeorm";
 import express from "express";
 import cors from "cors"
+import cookieParser from "cookie-parser";
 
 import userRoutes from "./modules/routes/user.route"
 import courseRoutes from "./modules/routes/course.route"
@@ -10,6 +11,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors())
+app.use(cookieParser())
 
 app.use('/api/user', userRoutes)
 app.use('/api/courses', courseRoutes)
