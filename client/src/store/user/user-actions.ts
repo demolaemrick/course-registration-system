7ec94e729d11799488c737bf3805d2bee0b52644
Emrick19 <emricky17@gmail.com>
2021-07-10 21:33:38 +0100

    Add login action

--- client/src/store/user/user-actions.ts
+++ client/src/store/user/user-actions.ts
@@ -1,17 +1,30 @@
-import * as apis from '../../api'
-import { userFormData } from "../../types/user"
+import * as apis from "../../api";
+import { userFormData, loginCredentials } from "../../types/user";
 import { AppDispatch } from "../index";
-import { userActions } from "./user-slice"
-import { History } from 'history';
+import { userActions } from "./user-slice";
+import { History } from "history";
 
-
-export const register = (formData: userFormData, router: History) => async (dispatch: AppDispatch) => {
-    console.log(formData)
-    try{
-        await apis.registerUser(formData);
-        router.push('/login')
-    }catch(err){
-        console.log(err.response.data.errors)
-        dispatch(userActions.register({errors: err.response.data.errors}))
+export const register =
+  (formData: userFormData, router: History) =>
+  async (dispatch: AppDispatch) => {
+    console.log(formData);
+    try {
+      await apis.registerUser(formData);
+      router.push("/login");
+    } catch (err) {
+      console.log(err.response.data.errors);
+      dispatch(userActions.register({ errors: err.response.data.errors }));
     }
-}
+  };
+
+export const login =
+  (credentials: loginCredentials, router: History) =>
+  async (dispatch: AppDispatch) => {
+    console.log(credentials);
+    try {
+      const response = await apis.login(credentials);
+      console.log(response);
+    } catch (err) {
+      console.log(err.response.data);
+    }
+  };
