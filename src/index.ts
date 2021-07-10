67f84140e1953149954f008386415da8c0eac7c2
Emrick19 <emricky17@gmail.com>
2021-07-10 21:34:29 +0100

    Add more properties to cors middleware

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,10 @@ import courseRoutes from "./modules/routes/course.route"
 const app = express();
 
 app.use(express.json());
-app.use(cors())
+app.use(cors({
+  origin: 'http://localhost:3000',
+  credentials: true
+}))
 app.use(cookieParser())
 
 app.use('/api/user', userRoutes)
