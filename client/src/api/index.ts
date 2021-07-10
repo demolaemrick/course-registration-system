f13865a6a51a585e5bb4ac3e4d1995178ee79c2d
Emrick19 <emricky17@gmail.com>
2021-07-10 21:04:10 +0100

    Add login route

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { userFormData } from "../types/user";
+import { userFormData, loginCredentials } from "../types/user";
 
 const API = axios.create({ baseURL: 'http://localhost:5000/api' });
 
@@ -8,3 +8,4 @@ export const fetchCourses = () => API.get(`/courses`);
 
 //authentication
 export const registerUser = (userData: userFormData) => API.post('/user/register', userData)
+export const login = (credentials: loginCredentials) => API.post('/user/auth', credentials, { withCredentials: true })
