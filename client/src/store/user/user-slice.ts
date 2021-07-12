4c5c4eed05ab9ebcfc90fa8486ecfe7714271bc3
Emrick19 <emricky17@gmail.com>
2021-07-12 23:28:25 +0100

    Change errors type

--- client/src/store/user/user-slice.ts
+++ client/src/store/user/user-slice.ts
@@ -1,13 +1,9 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { userFormData } from "../../types/user";
-
-// interface errorTypes {
-//   errors: userFormData[];
-// }
+import { errorMessage } from "../../types/user";
 
 interface userType {
   accessToken?: string | null;
-  errors: userFormData | {};
+  errors: errorMessage;
 }
 const initialState: userType = {
   accessToken: null,
