c710b60acd8b5bd51cce2d1c475c32901e5aca45
Emrick19 <emricky17@gmail.com>
2021-08-01 12:24:02 +0100

    Fix profile route

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -1,12 +1,12 @@
 import axios from 'axios';
 import { userFormData, loginCredentials } from "../types/user";
 
-const API = axios.create({ baseURL: 'http://localhost:5000/api' });
+const API = axios.create({ baseURL: 'http://localhost:5000/api', withCredentials: true  });
 
 //courses
 export const fetchCourses = () => API.get(`/courses`);
 
 //authentication
 export const registerUser = (userData: userFormData) => API.post('/user/register', userData)
-export const login = (credentials: loginCredentials) => API.post('/user/auth', credentials, { withCredentials: true })
-export const profile = () =>  API.get(`user/profile`)
+export const login = (credentials: loginCredentials) => API.post('/user/auth', credentials)
+export const profile = () =>  API.get(`/user/profile`)
