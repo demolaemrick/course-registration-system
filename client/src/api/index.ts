50896220d87bbd394ea0d13ce00751b398197cca
Emrick19 <emricky17@gmail.com>
2021-08-01 19:35:52 +0100

    Add logout route

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -9,4 +9,5 @@ export const fetchCourses = () => API.get(`/courses`);
 //authentication
 export const registerUser = (userData: userFormData) => API.post('/user/register', userData)
 export const login = (credentials: loginCredentials) => API.post('/user/auth', credentials)
+export const logout = () => API.get('/user/logout')
 export const profile = () =>  API.get(`/user/profile`)
