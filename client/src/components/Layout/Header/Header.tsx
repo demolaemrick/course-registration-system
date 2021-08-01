ee690ef115856810e8d0c90f3e948ba578c79e56
Emrick19 <emricky17@gmail.com>
2021-08-01 21:34:55 +0100

    Logically show navigation links

--- client/src/components/Layout/Header/Header.tsx
+++ client/src/components/Layout/Header/Header.tsx
@@ -1,11 +1,12 @@
 import { NavLink, useHistory } from "react-router-dom";
-import { useDispatch } from "react-redux"
-import { logout } from "../../../store/user/user-actions"
+import { useDispatch, useSelector } from "react-redux";
+import { logout } from "../../../store/user/user-actions";
+import { RootState } from "../../../store";
 
-
-import  classes from "./Header.module.css";
+import classes from "./Header.module.css";
 
 const Header = () => {
+  const { isLoggedIn } = useSelector((state: RootState) => state.userReducer);
   const dispatch = useDispatch();
   const history = useHistory();
   return (
@@ -13,10 +14,17 @@ const Header = () => {
       <h1>
         <NavLink to="/">Achievers</NavLink>
       </h1>
-      <div className={classes["header-items"]}>        
-        <NavLink to="/login">login</NavLink>
-        <NavLink to="/profile">profile</NavLink>
-        <NavLink to="/logout" onClick={() => dispatch(logout(history))}>logout</NavLink>
+      <div className={classes["header-items"]}>
+        {isLoggedIn ? (
+          <NavLink to="/logout" onClick={() => dispatch(logout(history))}>
+            logout
+          </NavLink>
+        ) : (
+          <>
+            <NavLink to="/login">login</NavLink>
+            <NavLink to="/profile">profile</NavLink>
+          </>
+        )}
       </div>
     </header>
   );
