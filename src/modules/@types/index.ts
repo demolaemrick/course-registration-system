59cfdff10013d6ebee0a9153ab68488806573310
Emrick19 <emricky17@gmail.com>
2021-08-01 12:25:48 +0100

    Change userId type

--- src/modules/@types/index.ts
+++ src/modules/@types/index.ts
@@ -2,7 +2,7 @@ import * as express from "express"
 declare global {
     namespace Express {
         interface Request {
-            userId? : Record<string,any>
+            userId? : string //Record<string,any>
         }
     }
 }
