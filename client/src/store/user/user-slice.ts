da6d314f39bea224321cb6443a32922b52adbc2f
Emrick19 <emricky17@gmail.com>
2021-08-15 22:10:18 +0100

    Add loading state

--- client/src/store/user/user-slice.ts
+++ client/src/store/user/user-slice.ts
@@ -5,11 +5,13 @@ interface userType {
   user?: User;
   errors?: errorMessage;
   isLoggedIn?: boolean;
+  isLoading?: boolean;
 }
 const initialState: userType = {
   user: {} as User,
   errors: {},
   isLoggedIn: false,
+  isLoading: false
 };
 
 export const userSlice = createSlice({
@@ -19,9 +21,13 @@ export const userSlice = createSlice({
     register: (state, action: PayloadAction<userType>) => {
       state.errors = action.payload.errors;
     },
+    authLoadStart: (state) => {
+      state.isLoading = true;
+    },
     login: (state, action: PayloadAction<userType>) => {
       state.user = action.payload.user;
       state.isLoggedIn = true;
+      state.isLoading = false;
     },
     logout: (state) => {
       state.isLoggedIn = false;
