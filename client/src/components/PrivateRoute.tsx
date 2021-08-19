a1eafddef9a1a7d8b6c715648091bb4e46fff77c
Emrick19 <emricky17@gmail.com>
2021-08-19 21:50:17 +0100

    Change component extension

---
+++ client/src/components/PrivateRoute.tsx
@@ -0,0 +1,19 @@
+import { Route, Redirect } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+import { RootState } from "../store";
+
+const PrivateRoute = ({ component: Component, ...rest }: any) => {
+  const { isLoggedIn } = useSelector((state: RootState) => state.userReducer);
+
+  return (
+    <Route
+      {...rest}
+      render={(props) => {
+        return isLoggedIn ? <Component {...props} /> : <Redirect to="/login" />;
+      }}
+    />
+  );
+};
+
+export default PrivateRoute;
