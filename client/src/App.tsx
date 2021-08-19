a4b0083fa759580a18827b45bfcf3b19bbb4d189
Emrick19 <emricky17@gmail.com>
2021-08-19 21:49:50 +0100

    Update App component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,31 +1,33 @@
-import { useEffect } from "react";
+import { useEffect } from "react"
 import { BrowserRouter as Router, Switch, Route,  } from "react-router-dom";
-import { useDispatch } from "react-redux";
+import { useDispatch } from "react-redux"
+
 
 import Layout from "./components/Layout/Layout";
 import Home from "./components/pages/Home";
 import Login from "./components/pages/Login";
 import SignUp from "./components/pages/SignUp";
 import Profile from "./components/pages/Profile";
-import PrivateRoute from "./components/PrivateRoute";  
+import PrivateRoute from "./components/PrivateRoute"; 
+
+import { checkUser } from "./store/user/user-actions"
 
 
-import { getUserProfile } from "./store/user/user-actions";
 
 function App() {
+
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(getUserProfile());
-  }, [dispatch]);
-
+    dispatch(checkUser())
+  },[dispatch])
   return (
     <Router>
       <Switch>
         <Route path="/register" component={SignUp} />
         <Route path="/login" component={Login} />
         <Layout>
-          <Route exact path="/" component={Home} />
+          <PrivateRoute exact path="/" component={Home} />
           <PrivateRoute path="/profile" component={Profile} />
         </Layout>
       </Switch>
