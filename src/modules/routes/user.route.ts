78acdcaea1c5b2c90d1f0a114788672d17460f2d
Emrick19 <emricky17@gmail.com>
2021-08-28 23:11:01 +0100

    Add cloudinary middleware to updateProfile route

--- src/modules/routes/user.route.ts
+++ src/modules/routes/user.route.ts
@@ -8,14 +8,15 @@ import {
   loginValidationRules,
 } from "../utils/validator";
 
-import authorization from "../utils/authorization";
+import authorization from "../middlewares/authorization";
+import parser from "../middlewares/cloudinary"
 
 const router = Router();
 
 router.post("/register", registerValidationRules(), validate, createUser);
 router.post("/auth", loginValidationRules(), validate, login);
 router.get("/logout", logout);
-router.patch("/:id/update", updateUser);
+router.patch("/update", parser.single('profile_picture'), authorization, updateUser);
 router.get("/profile", authorization, getUserProfile);
 
 // router.get("/protected", authorization, (req, res) => {
