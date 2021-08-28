665d0f40aaba4b601a256da9747071e2151e004e
Emrick19 <emricky17@gmail.com>
2021-08-28 21:05:38 +0100

    Change the input accept value

--- client/src/components/UI/Buttons/FileButton/FileButton.tsx
+++ client/src/components/UI/Buttons/FileButton/FileButton.tsx
@@ -1,10 +1,15 @@
-import classes from "./FileButton.module.css"
+import classes from "./FileButton.module.css";
 
 const FileButton = (props: any) => {
   return (
     <div className={classes["upload-btn-wrapper"]}>
       <button className={classes.btn}>{props.children}</button>
-      <input type="file" name="myfile" accept=".png, .jpg, .jpeg" onChange={props.change} />
+      <input
+        type="file"
+        name="myfile"
+        accept="image/*"
+        onChange={props.change}
+      />
     </div>
   );
 };
