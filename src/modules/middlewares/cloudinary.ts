19d71099672d26dbc40f12b9d6c1a8d0a7a2ad1e
Emrick19 <emricky17@gmail.com>
2021-08-28 21:05:59 +0100

    A config file for cloudinary

---
+++ src/modules/middlewares/cloudinary.ts
@@ -0,0 +1,31 @@
+import multer from "multer";
+import cloudinary from "cloudinary";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+
+let cloud = cloudinary.v2
+
+const { CLOUDINARY_HOST, CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET } =
+  process.env;
+
+cloud.config({
+  cloud_name: CLOUDINARY_HOST,
+  api_key: CLOUDINARY_API_KEY,
+  api_secret: CLOUDINARY_API_SECRET,
+  secure: true,
+});
+
+const storage = new CloudinaryStorage({
+  cloudinary: cloud,
+  params: async (req, file) => {
+  
+    return {
+      folder: 'Dev',
+      format: 'png',
+      public_id: file.filename
+    };
+  },
+});
+
+const parser = multer({ storage });
+
+export default parser;
