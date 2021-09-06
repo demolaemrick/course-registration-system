6316eddae81b6d0bb7efb9000af1a0d62a32aadf
Emrick19 <emricky17@gmail.com>
2021-09-06 16:22:16 +0100

    Change some imports

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -1,14 +1,20 @@
-import axios from 'axios';
-import { userFormData, loginCredentials } from "../types/user";
+import axios from "axios";
+import { RegisterFormData, LoginCredentials } from "../types/user";
 
-const API = axios.create({ baseURL: 'http://localhost:5000/api', withCredentials: true  });
+const API = axios.create({
+  baseURL: "http://localhost:5000/api",
+  withCredentials: true,
+});
 
 //courses
 export const fetchCourses = () => API.get(`/courses`);
 
 //authentication
-export const registerUser = (userData: userFormData) => API.post('/user/register', userData)
-export const login = (credentials: loginCredentials) => API.post('/user/auth', credentials)
-export const logout = () => API.get('/user/logout')
-export const profile = () =>  API.get(`/user/profile`)
-export const updateProfile = (userData: any) => API.patch('/user/update', userData)
+export const registerUser = (userData: RegisterFormData) =>
+  API.post("/user/register", userData);
+export const login = (credentials: LoginCredentials) =>
+  API.post("/user/auth", credentials);
+export const logout = () => API.get("/user/logout");
+export const profile = () => API.get(`/user/profile`);
+export const updateProfile = (userData: any) =>
+  API.patch("/user/update", userData);
