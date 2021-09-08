ac0416b8e02e977808c5ce481059e3d4dacc79e7
Emrick19 <emricky17@gmail.com>
2021-09-08 21:15:13 +0100

    Add pagination and selection

--- client/src/components/UI/Table2.tsx
+++ client/src/components/UI/Table2.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import {
   withStyles,
   Theme,
@@ -12,8 +13,9 @@ import {
   TableHead,
   TableRow,
   Paper,
+  TablePagination,
+  Checkbox,
 } from "@material-ui/core";
-import { useSelector } from "react-redux";
 import { Courses } from "../../types/course";
 
 const StyledTableCell = withStyles((theme: Theme) =>
@@ -31,9 +33,9 @@ const StyledTableCell = withStyles((theme: Theme) =>
 const StyledTableRow = withStyles((theme: Theme) =>
   createStyles({
     root: {
-      "&:nth-of-type(odd)": {
-        backgroundColor: theme.palette.action.hover,
-      },
+    //   "&:nth-of-type(odd)": {
+    //     backgroundColor: theme.palette.action.hover,
+    //   },
     },
   })
 )(TableRow);
@@ -47,32 +49,129 @@ const useStyles = makeStyles({
 export default function CustomizedTables({ courses }: Courses) {
   const classes = useStyles();
 
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+  const [selected, setSelected] = useState<string[]>([]);
+
+  const numSelected = selected.length;
+  const rowCount = courses.length;
+
+  const isSelected = (name: string) => selected.indexOf(name) !== -1;
+
+  const emptyRows =
+    rowsPerPage - Math.min(rowsPerPage, courses.length - page * rowsPerPage);
+
+  const handleChangePage = (event: unknown, newPage: number) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  const handleClick = (event: React.MouseEvent<unknown>, name: string) => {
+    const selectedIndex = selected.indexOf(name);
+    let newSelected: string[] = [];
+
+    if (selectedIndex === -1) {
+      newSelected = newSelected.concat(selected, name);
+    } else if (selectedIndex === 0) {
+      newSelected = newSelected.concat(selected.slice(1));
+    } else if (selectedIndex === selected.length - 1) {
+      newSelected = newSelected.concat(selected.slice(0, -1));
+    } else if (selectedIndex > 0) {
+      newSelected = newSelected.concat(
+        selected.slice(0, selectedIndex),
+        selected.slice(selectedIndex + 1),
+      );
+    }
+
+    setSelected(newSelected);
+  };
+
+  const handleSelectAllClick = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.checked) {
+      const newSelecteds = courses.map((n) => n.course_title);
+      setSelected(newSelecteds);
+      return;
+    }
+    setSelected([]);
+  };
+
   return (
-    <TableContainer component={Paper}>
-      <Table className={classes.table} aria-label="customized table">
-        <TableHead>
-          <TableRow>
-            <StyledTableCell>Course code</StyledTableCell>
-            <StyledTableCell>Course title</StyledTableCell>
-            <StyledTableCell align="right">Course unit</StyledTableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {courses.map((course) => (
-            <StyledTableRow key={course.uuid}>
-              <StyledTableCell component="th" scope="course">
-                {course.course_code}
+    <div>
+      <TableContainer component={Paper}>
+        <Table className={classes.table} aria-label="customized table">
+          <TableHead>
+            <TableRow>
+              <StyledTableCell>ID</StyledTableCell>
+              <StyledTableCell>Course title</StyledTableCell>
+              <StyledTableCell>Course code</StyledTableCell>
+              <StyledTableCell>Course unit</StyledTableCell>
+              <StyledTableCell padding="checkbox">
+                <Checkbox
+                  indeterminate={numSelected > 0 && numSelected < rowCount}
+                  checked={rowCount > 0 && numSelected === rowCount}
+                  onChange={handleSelectAllClick}
+                  inputProps={{ "aria-label": "select all desserts" }}
+                />
               </StyledTableCell>
-              <StyledTableCell>
-                {course.course_title}
-              </StyledTableCell>
-              <StyledTableCell align="right">
-                {course.course_unit}
-              </StyledTableCell>
-            </StyledTableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {courses
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((course, index) => {
+                const isItemSelected = isSelected(course.course_title);
+                const labelId = `enhanced-table-checkbox-${index}`;
+                return (
+                  <StyledTableRow
+                  key={course.uuid}
+                  hover
+                  onClick={(event) => handleClick(event, course.course_title)}
+                  role="checkbox"
+                  aria-checked={isItemSelected}
+                  tabIndex={-1}
+                  selected={isItemSelected}                  
+                   >
+                    <StyledTableCell>{index + 1}</StyledTableCell>
+                    <StyledTableCell component="th" scope="course">
+                      {course.course_code}
+                    </StyledTableCell>
+                    <StyledTableCell>{course.course_title}</StyledTableCell>
+                    <StyledTableCell>
+                      {course.course_unit}
+                    </StyledTableCell>
+
+                    <TableCell padding="checkbox">
+                      <Checkbox
+                        checked={isItemSelected}
+                        inputProps={{ "aria-labelledby": labelId }}
+                      />
+                    </TableCell>
+                  </StyledTableRow>
+                );
+              })}
+          </TableBody>
+          {emptyRows > 0 && (
+            <TableRow style={{ height: 53 * emptyRows }}>
+              <TableCell colSpan={6} />
+            </TableRow>
+          )}
+        </Table>
+        <TablePagination
+          rowsPerPageOptions={[]}
+          component="div"
+          count={courses.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+        />
+      </TableContainer>
+    </div>
   );
 }
