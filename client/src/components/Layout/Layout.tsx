859a9a5a963f3afed2b3d09bb12af383b39e692d
Emrick19 <emricky17@gmail.com>
2021-09-15 22:57:17 +0100

    Use React Fragment

--- client/src/components/Layout/Layout.tsx
+++ client/src/components/Layout/Layout.tsx
@@ -4,10 +4,10 @@ import Header from "./Header/Header";
 
 const Layout: React.FC = (props) => {
   return (
-    <>
+    <React.Fragment>
       <Header />
       <main>{props.children}</main>
-    </>
+    </React.Fragment>
   );
 };
 
