076b079bb300a691ec341599970722b3807bbd31
Emrick19 <emricky17@gmail.com>
2021-09-19 18:25:59 +0100

    Modify registerCourse controller

--- src/modules/controllers/course.controller.ts
+++ src/modules/controllers/course.controller.ts
@@ -1,19 +1,23 @@
-import { Request, Response } from "express"
-import Course from "../../entity/Course"
-
+import { Request, Response } from "express";
+import Course from "../../entity/Course";
+import EnrolledCourse from "../../entity/EnrolledCourse";
 
 export const getCourses = async (_, res: Response) => {
-    try{
-        const courses = await Course.find()
+  try {
+    const courses = await Course.find();
 
-        return res.send(courses)
-    }catch(err){
-        res.status(500).send(err.message)
-    }
-}
+    return res.send(courses);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+};
 
-export const registerCourses = async(req: Request, res: Response) => {
-    try{
+export const registerCourses = async (req: Request, res: Response) => {
+  try {
+    const result = await EnrolledCourse.insert(req.body);
 
-    }catch(err){}
-}
+    return res.send(result);
+  } catch (err) {
+    return res.status(500).send(err.message);
+  }
+};
