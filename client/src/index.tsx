1c15fff1a3e07e34a09ae037995fcc493e716bcc
Emrick19 <emricky17@gmail.com>
2021-09-20 19:53:11 +0100

    Customize theme

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,17 +1,39 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { ChakraProvider } from "@chakra-ui/react";
-import { store } from "./store";
 import { Provider } from "react-redux";
+
+import { ChakraProvider } from "@chakra-ui/react";
+
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { pink } from "@mui/material/colors";
+
+import { store } from "./store";
 
 import "./index.css";
 import App from "./App";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      // light: teal[200],
+      main: "#004c23",
+      // dark: teal[800],
+    },
+    secondary: {
+      light: pink[200],
+      main: pink[700],
+      dark: pink[900],
+    },
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
     <ChakraProvider>
       <Provider store={store}>
-        <App />
+        <ThemeProvider theme={theme}>
+          <App />
+        </ThemeProvider>
       </Provider>
     </ChakraProvider>
   </React.StrictMode>,
