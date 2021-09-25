436820752da0955d864ee9f0cc717a520d127c5d
Emrick19 <emricky17@gmail.com>
2021-09-25 18:33:50 +0100

    Remove comment

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,8 +4,7 @@ import { useDispatch } from "react-redux";
 
 import Layout from "./components/Layout/Layout";
 import Home from "./components/pages/Home";
-// import Login from "./components/pages/Login";
-import Login from "./components/pages/NewLogin";
+import Login from "./components/pages/Login";
 import SignUp from "./components/pages/SignUp";
 import Profile from "./components/pages/Profile";
 import PrivateRoute from "./components/PrivateRoute";
