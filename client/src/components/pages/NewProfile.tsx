f8537d127c5f3451f2a875c39fa74500a3647633
Emrick19 <emricky17@gmail.com>
2021-09-30 21:59:10 +0100

    Update page

--- client/src/components/pages/NewProfile.tsx
+++ client/src/components/pages/NewProfile.tsx
@@ -1,4 +1,12 @@
-import { Fragment } from "react";
+import { useState, FormEvent, ChangeEvent } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
+
+import { RootState } from "../../store";
+import { updateUser } from "../../store/user/user-actions";
+
+import { UserUpdateOptions } from "../../types/user";
+
 import {
   Grid,
   Box,
@@ -8,10 +16,10 @@ import {
   FormLabel,
   OutlinedInput,
   Stack,
-  Select,
   MenuItem,
   Avatar,
   Button,
+  TextField,
 } from "@mui/material";
 
 import UploadButton from "../UI/Buttons/UploadButton";
@@ -32,6 +40,57 @@ const selects = [
   { id: "level", name: "LEVEL", values: [100, 200, 300, 400, 500] },
 ];
 const Profile = () => {
+  const { user, doesNotHaveCompleteProfile } = useSelector(
+    (state: RootState) => state.userReducer
+  );
+
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+  const [userData, setUserData] = useState<UserUpdateOptions>({
+    gender: "",
+    college: "",
+    phone: "",
+    level: "",
+    department: "",
+    programme: "",
+    profile_picture: "",
+  });
+
+  const [imageSrc, setImageSrc] = useState(AvatarImage);
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.currentTarget;
+
+    setUserData({
+      ...userData,
+      [name]: value,
+    });
+  };
+
+  const handlePhotoChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const fileList = event.target.files;
+
+    if (!fileList) return;
+
+    setUserData({ ...userData, profile_picture: fileList[0] });
+    setImageSrc(URL.createObjectURL(fileList[0]));
+  };
+
+  const handleProfileUpdate = (e: FormEvent) => {
+    e.preventDefault();
+
+    const formData = new FormData();
+    formData.append("profile_picture", userData.profile_picture);
+    formData.append("gender", userData.gender);
+    formData.append("phone", userData.phone);
+    formData.append("level", userData.level);
+    formData.append("college", userData.college);
+    formData.append("department", userData.department);
+    formData.append("programme", userData.programme);
+
+    dispatch(updateUser(formData, history));
+  };
   return (
     <Box
       component="form"
@@ -43,7 +102,7 @@ const Profile = () => {
       }}
       noValidate
       autoComplete="off"
-      // onSubmit={handleSubmit}
+      onSubmit={handleProfileUpdate}
     >
       <Paper
         elevation={4}
@@ -57,7 +116,7 @@ const Profile = () => {
             <Box sx={{ backgroundColor: "white", padding: 2, minHeight: 315 }}>
               <Stack direction="column">
                 {inputFields.slice(0, 3).map((input) => (
-                  <FormControl sx={{ mb: 2 }} size="small">
+                  <FormControl key={input.id} sx={{ mb: 2 }} size="small">
                     <Box
                       sx={{
                         display: "flex",
@@ -69,13 +128,18 @@ const Profile = () => {
                       <FormLabel htmlFor={input.id} sx={{ width: 100 }}>
                         {input.name}
                       </FormLabel>
-                      <OutlinedInput id={input.id} type="text" />
+                      <OutlinedInput
+                        id={input.id}
+                        type="text"
+                        name={input.name}
+                        onChange={handleChange}
+                      />
                     </Box>
                   </FormControl>
                 ))}
 
                 {selects.map((select) => (
-                  <FormControl sx={{ mb: 2 }} size="small">
+                  <FormControl key={select.id} sx={{ mb: 2 }} size="small">
                     <Box
                       sx={{
                         display: "flex",
@@ -86,18 +150,28 @@ const Profile = () => {
                         {select.name}
                       </FormLabel>
 
-                      <Select
+                      <TextField
+                        select
                         name={select.id}
-                        input={<OutlinedInput />}
+                        variant="outlined"
+                        onChange={handleChange}
                         sx={{ minWidth: 210 }}
+                        size="small"
+                        value={
+                          select.id === "gender"
+                            ? userData.gender
+                            : userData.level
+                        }
                       >
                         <MenuItem disabled value="">
                           <em>Placeholder</em>
                         </MenuItem>
-                        {select.values.map((value) => (
-                          <MenuItem value={value}>{value}</MenuItem>
+                        {select.values.map((value, index) => (
+                          <MenuItem key={index} value={value}>
+                            {value}
+                          </MenuItem>
                         ))}
-                      </Select>
+                      </TextField>
                     </Box>
                   </FormControl>
                 ))}
@@ -111,7 +185,7 @@ const Profile = () => {
             <Box sx={{ backgroundColor: "white", padding: 2, minHeight: 315 }}>
               <Stack direction="column">
                 {inputFields.slice(4, 6).map((input) => (
-                  <FormControl sx={{ mb: 2 }} size="small">
+                  <FormControl key={input.id} sx={{ mb: 2 }} size="small">
                     <Box
                       sx={{
                         display: "flex",
@@ -137,20 +211,22 @@ const Profile = () => {
             <Box sx={{ backgroundColor: "white", padding: 2, minHeight: 315 }}>
               <Avatar
                 sx={{ width: 200, height: 150, mb: 2 }}
-                src={AvatarImage}
+                src={imageSrc}
                 variant="square"
                 alt=""
               />
-              <Box
-                sx={{
-                  display: "flex",
-                  flexDirection: "column",
-                  alignItems: "center",
-                }}
-              >
-                <UploadButton>Upload</UploadButton>
-                <Button variant="contained">Submit</Button>
-              </Box>
+              {doesNotHaveCompleteProfile && (
+                <Box
+                  sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                  }}
+                >
+                  <UploadButton change={handlePhotoChange}>Upload</UploadButton>
+                  <Button variant="contained">Submit</Button>
+                </Box>
+              )}
             </Box>
           </Grid>
         </Grid>
