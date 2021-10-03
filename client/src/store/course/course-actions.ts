987cbd740a1e1f7e0726334becc558e320c16ef5
Emrick19 <emricky17@gmail.com>
2021-10-03 23:35:02 +0100

    Dispatch fetchCourseStart

--- client/src/store/course/course-actions.ts
+++ client/src/store/course/course-actions.ts
@@ -2,10 +2,13 @@ import { courseActions } from "./course-slice";
 import { AppDispatch } from "../index";
 import * as apis from "../../api";
 
-export const fetchCourses = () => async (dispatch: AppDispatch) => {  
+import { Course } from "../../types/course";
+
+export const fetchCourses = () => async (dispatch: AppDispatch) => {
+  dispatch(courseActions.fetchCourseStart());
   try {
     const { data: courses } = await apis.fetchCourses();
-    dispatch(courseActions.replaceCourses({ courses: courses }));
+    dispatch(courseActions.fetchCourses({ courses: courses as Course[] }));
   } catch (err) {
     console.log(err);
   }
