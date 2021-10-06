471ab6482e8a8d881aa575e840a69f194f1c833d
Emrick19 <emricky17@gmail.com>
2021-10-06 19:49:13 +0100

    Add border to the table

--- client/src/components/pages/Enrolled.tsx
+++ client/src/components/pages/Enrolled.tsx
@@ -47,52 +47,51 @@ const Enrolled = () => {
         elevation={10}
         sx={{ padding: 4, backgroundColor: "#c9d6d1", marginTop: 5 }}
       >
-        <Box>
-          <TableContainer component={Paper}>
-            <Toolbar
-              sx={{
-                borderBottom: "1px solid #c9d6d1",
-                backgroundColor: "#f2f7f5",
-              }}
-            >
-              <Typography variant="subtitle1">Enroll History</Typography>
-            </Toolbar>
-            <TableContainer sx={{ padding: 2 }} component={Paper}>
-              <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                <TableHead>
-                  <TableRow>
-                    <TableCell>Course Title</TableCell>
-                    <TableCell align="left">Session</TableCell>
-                    <TableCell align="left">Department</TableCell>
-                    <TableCell align="left">Level</TableCell>
-                    <TableCell align="left">Semester</TableCell>
-                    <TableCell align="left">Enrollment Date</TableCell>
-                    <TableCell align="left">Action</TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {rows.map((row) => (
-                    <TableRow key={row.name} sx={{ " td, th": { border: 0 } }}>
-                      <TableCell component="th" scope="row">
-                        {row.name}
-                      </TableCell>
-                      <TableCell align="left">{row.session}</TableCell>
-                      <TableCell align="left">{row.department}</TableCell>
-                      <TableCell align="left">{row.level}</TableCell>
-                      <TableCell align="left">{row.semester}</TableCell>
-                      <TableCell align="left">{row.date}</TableCell>
-                      <TableCell align="left">
-                        <Button variant="contained" color="primary">
-                          Print
-                        </Button>
-                      </TableCell>
-                    </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-            </TableContainer>
-          </TableContainer>
-        </Box>
+        <Toolbar
+          sx={{
+            borderBottom: "1px solid #c9d6d1",
+            backgroundColor: "#f2f7f5",
+          }}
+        >
+          <Typography variant="subtitle1">Enroll History</Typography>
+        </Toolbar>
+        <TableContainer sx={{ padding: 2 }} component={Paper}>
+          <Table
+            sx={{ minWidth: 650, border: "1px solid #c9d6d1" }}
+            aria-label="simple table"
+          >
+            <TableHead>
+              <TableRow>
+                <TableCell>Course Title</TableCell>
+                <TableCell align="left">Session</TableCell>
+                <TableCell align="left">Department</TableCell>
+                <TableCell align="left">Level</TableCell>
+                <TableCell align="left">Semester</TableCell>
+                <TableCell align="left">Enrollment Date</TableCell>
+                <TableCell align="left">Action</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map((row) => (
+                <TableRow key={row.name} sx={{ " td, th": { border: 0 } }}>
+                  <TableCell component="th" scope="row">
+                    {row.name}
+                  </TableCell>
+                  <TableCell align="left">{row.session}</TableCell>
+                  <TableCell align="left">{row.department}</TableCell>
+                  <TableCell align="left">{row.level}</TableCell>
+                  <TableCell align="left">{row.semester}</TableCell>
+                  <TableCell align="left">{row.date}</TableCell>
+                  <TableCell align="left">
+                    <Button variant="contained" color="primary">
+                      Print
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
       </Paper>
     </Container>
   );
