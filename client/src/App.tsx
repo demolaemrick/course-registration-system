a1606faa5aa23bd31a0b2bc838dcb6dab91b8d38
Emrick19 <emricky17@gmail.com>
2021-10-07 22:12:05 +0100

    Import MangeAccount

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -12,7 +12,8 @@ import Enrolled from "./components/pages/Enrolled";
 
 import { checkUser } from "./store/user/user-actions";
 
-import AdminDashboard from "./components/pages/AdminDdashboard";
+import AdminDashboard from "./components/pages/AdminDashboard";
+import ManageAccount from "./components/pages/ManageAccount";
 
 function App() {
   const dispatch = useDispatch();
@@ -29,7 +30,8 @@ function App() {
           <PrivateRoute path="/courses/enroll-history" component={Enrolled} />
           <Route path="/register" component={SignUp} />
           <Route path="/login" component={Login} />
-          <Route path="/admin" component={AdminDashboard} />
+          <Route exact path="/admin" component={AdminDashboard} />
+          <Route path="/admin/manage-students" component={ManageAccount} />
         </Layout>
       </Switch>
     </Router>
