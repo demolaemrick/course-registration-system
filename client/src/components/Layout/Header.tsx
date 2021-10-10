2a854aaefd31ff348b1af458c0d4ff5dee38950c
Emrick19 <emricky17@gmail.com>
2021-10-10 22:09:24 +0100

    Update component

--- client/src/components/Layout/Header.tsx
+++ client/src/components/Layout/Header.tsx
@@ -13,40 +13,51 @@ export default function ButtonAppBar() {
   const dispatch = useDispatch();
   const history = useHistory();
 
-  const path = history.location.pathname;
+  const pathname = history.location.pathname;
 
-  const pathIsLogin = path === "/login";
+  const pathIsLogin = pathname === "/login";
+
+  const path = pathIsLogin ? "register" : "login";
 
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="fixed">
         <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            <NavLink to="/">
-              <Box sx={{ width: 50, height: 50 }}>
-                <Logo />
-              </Box>
-            </NavLink>
+          <Typography
+            variant="h6"
+            component={NavLink}
+            to="/"
+            sx={{ flexGrow: 1 }}
+          >
+            <Box sx={{ width: 50, height: 50 }}>
+              <Logo />
+            </Box>
           </Typography>
           {isLoggedIn ? (
             <Fragment>
-              <Button color="inherit">
-                <NavLink to="/profile">profile</NavLink>
+              <Button component={NavLink} to="/profile" color="inherit">
+                profile
               </Button>
-              <Button color="inherit">
-                <NavLink to="/courses/enroll-history">Enrolled courses</NavLink>
+
+              <Button
+                component={NavLink}
+                to="/courses/enroll-history"
+                color="inherit"
+              >
+                Enrolled courses
               </Button>
-              <Button color="inherit" onClick={() => dispatch(logout(history))}>
-                <NavLink to="/logout">logout</NavLink>
+              <Button
+                component={NavLink}
+                to="/logout"
+                color="inherit"
+                onClick={() => dispatch(logout(history))}
+              >
+                logout
               </Button>
             </Fragment>
           ) : (
-            <Button color="inherit">
-              {pathIsLogin ? (
-                <NavLink to="/register">Register</NavLink>
-              ) : (
-                <NavLink to="/login">login</NavLink>
-              )}
+            <Button component={NavLink} to={`/${path}`} color="inherit">
+              {path}
             </Button>
           )}
         </Toolbar>
